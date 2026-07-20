6abd5545af77e1ab916f8035d6cab6265d67e54c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:18:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,14 @@
+return {
+    ["Auto-complete"] = { fr = "Terminer automatiquement" },
+    ["Auto-played %1 card(s)."] = { fr = "%1 carte(s) jouée(s) automatiquement." },
+    ["Classic Klondike solitaire card game."] = { fr = "Jeu de cartes classique Solitaire Klondike." },
+    ["Draw %1"] = { fr = "Piocher %1" },
+    ["Draw 1"] = { fr = "Piocher 1" },
+    ["Draw 3"] = { fr = "Piocher 3" },
+    ["Draw mode"] = { fr = "Mode de pioche" },
+    ["Moves: %1  Foundations: %2/52"] = { fr = "Coups : %1  Fondations : %2/52" },
+    ["No automatic moves available."] = { fr = "Aucun coup automatique disponible." },
+    ["Solitaire"] = { fr = "Solitaire" },
+    ["Solved in %1 move(s)!"] = { fr = "Résolu en %1 coup(s) !" },
+    ["You won in %1 moves!"] = { fr = "Vous avez gagné en %1 coups !" },
+}
