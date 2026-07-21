8b4c73c3024ac66565e389c47870af50ef3d4d9c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 15:48:50 +0200

    chore: bump version to 1.0.2

--- _meta.lua
+++ _meta.lua
@@ -2,7 +2,7 @@ local _ = require("gettext")
 
 return {
     name        = "solitaire",
-    version     = "1.0.1",
+    version     = "1.0.2",
     fullname    = _("Solitaire"),
     description = _("Classic Klondike solitaire card game."),
 }
